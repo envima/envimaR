f1f55de4eace80e1ac1e6bc7750e66d8605d4548
tnauss <nauss@staff.uni-marburg.de>
2018-11-11 17:34:22 +0100

    Revert "Commit"
    This reverts commit 0098c415f9f07cc08c55889c4d5f2d282c1c50a7.

--- R/createEnvi.R
+++ R/createEnvi.R
@@ -7,11 +7,9 @@
 #' @param folders list of subfolders within the project directory.
 #' @param folder_names names of the variables that point to subfolders. If not
 #' provided, the base paths of the folders is used.
-#' @param git_repository name of the project's git repository. Will be
-#' added to the folders and subfolders defined in default "lut" or supplied by
-#' user will be created.
-#' @param git_folders subdirectories within git repository that should be
-#' created.
+#' @param git_name name of the project's git repository. In "lut" mode, it will
+#' be automatically added to the folder and subfolders src, fcts will be
+#' created. DOES NOT WORK YET!
 #' @param path_prefix a prefix for the folder names.
 #' @param global logical: export path strings as global variables?
 #' @param libs  vector with the  names of libraries
@@ -44,8 +42,7 @@
 #'}
 
 createEnvi = function(root_folder = tempdir(), folders = c("data", "data/tmp"),
-                      folder_names = NULL, git_repository = NULL,
-                      git_folders = NULL,
+                      folder_names = NULL, git_name = NULL,
                       path_prefix = "path_", global = FALSE,
                       libs = NULL,
                       fcts_folder = NULL, source_functions = !is.null(fcts_folder),
@@ -67,10 +64,6 @@ createEnvi = function(root_folder = tempdir(), folders = c("data", "data/tmp"),
                                 alt_env_root_folder = alt_env_root_folder)
 
   # Compile and create folders if necessary
-  if(!is.null(git_repository)){
-    folders = addGitFolders(folders = folders, git_repository = git_repository,
-                            git_folders = git_folders, lut_mode = lut_mode)
-  }
   folders = createFolders(root_folder, folders,
                           folder_names = folder_names, path_prefix = path_prefix)
 
--- R/pckgDefaults.R
+++ R/pckgDefaults.R
@@ -20,7 +20,7 @@ pckgDefaults = function(){
     folders = c("data", "data/tmp",
                 "doc", "log"),
 
-    git_subfolders = c("src", "doc", "fcts"),
+    git_subfolders = c("src", "fcts"),
 
     path_prefix = "path_",
 
